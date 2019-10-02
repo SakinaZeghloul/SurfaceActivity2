72a7c91b01e7ea54e1f5aa742919c1bb8049a36f
sino6@free.fr <9-oiTh3m/SY$@PJ7cHLIYo7W0^Qe>
2019-10-02 10:52:06 +0200

    Update Accueil_nouveauJeu.java

--- app/src/main/java/openclass/tp/nfa024/cnam/fr/surfaceactivity/Contrôleur/Accueil_nouveauJeu.java
+++ app/src/main/java/openclass/tp/nfa024/cnam/fr/surfaceactivity/Contrôleur/Accueil_nouveauJeu.java
@@ -1,32 +1,34 @@
 package openclass.tp.nfa024.cnam.fr.surfaceactivity.Contrôleur;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.preference.PowerPreference;
 
-import java.lang.reflect.Type;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Map;
 
 import openclass.tp.nfa024.cnam.fr.surfaceactivity.Modèle.POI;
-import openclass.tp.nfa024.cnam.fr.surfaceactivity.Modèle.POIHash;
 import openclass.tp.nfa024.cnam.fr.surfaceactivity.R;
 
 public class Accueil_nouveauJeu extends AppCompatActivity {
 
 
     private Button nouveauJeu, chargerJeu;
-    private HashMap<String, POI> mPOIHashMap, hashMapPOI;
-    POI mPOI;
+    private HashMap<String, POI> mPOIHashMap;
+    private Scanner mScanner;
+    private POI mPOI;
 
-
-    private static final String SHARED_PREFS = "SharedPref";
 
     private static final String TAG = "Accueil_nouveau_jeu";
 
@@ -39,13 +41,23 @@ public class Accueil_nouveauJeu extends AppCompatActivity {
         this.chargerJeu = (Button) this.findViewById(R.id.btn2);
 
         mPOI = new POI();
-        HashMap<String, POI> mPOIHashMap = POIHash.getPOIHash();
-        hashMapPOI=new HashMap<>();
+
+        mPOIHashMap = new HashMap<>();
 
 
         nouveauJeu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mPOIHashMap=readFile();
+                PowerPreference.getDefaultFile().setMap("POIHash", mPOIHashMap);
+
+                for (Map.Entry<String, POI> e : mPOIHashMap.entrySet()) {
+                    mPOI=e.getValue();
+
+                    Log.d(TAG, "POI: " + mPOI.getId());
+                    Log.d(TAG, "POI tag: " + mPOI.getTag());
+                    Log.d(TAG, "POI latitude: " + mPOI.getLatitude());
+                }
                 Intent map = new Intent(v.getContext(), Enigme.class);
                 startActivity(map);
             }
@@ -55,35 +67,65 @@ public class Accueil_nouveauJeu extends AppCompatActivity {
         chargerJeu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadData();
+                mPOIHashMap=PowerPreference.getDefaultFile().getMap("POIHash", HashMap.class, String.class, POI.class);
                 Intent i = new Intent(v.getContext(), Enigme.class);
-                String loadGame="Chargement des données";
+                String loadGame = "Chargement des données";
                 i.putExtra("Load Game", loadGame);
                 startActivity(i);
             }
         });
     }
 
+    public String readJsonFile() {
 
-    public HashMap<String, POI> loadData() {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("POI.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
 
-        SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences(SHARED_PREFS, this.MODE_PRIVATE);
+            Log.d(TAG, "Json: " + json);
 
-        Gson gson=new Gson();
-        String json=sharedPreferences.getString("KeyPOI", null);
-
-        Type token = new TypeToken<HashMap<String,POI>>(){}.getType();
-
-        hashMapPOI=gson.fromJson(json, token);
-
-        if(hashMapPOI==null){
-            hashMapPOI=new HashMap<>();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
-        return hashMapPOI;
+        return json;
     }
 
 
+    public HashMap<String, POI> readFile(){
 
+        String json=readJsonFile();
+
+        try {
+
+            JSONObject jsonObject=new JSONObject(json);
+            JSONArray jsonArray = jsonObject.getJSONArray("POI");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+                JSONObject poi = jsonArray.getJSONObject(i);
+
+                System.out.println(poi);
+
+                String ID=poi.getString("id");
+                int tag=poi.getInt("tag");
+                double latitude=poi.getDouble("latitude");
+                double longitude=poi.getDouble("longitude");
+                int capture=poi.getInt("capture");
+                int min=poi.getInt("min");
+                int max=poi.getInt("max");
+
+                mPOIHashMap.put(ID, new POI(ID, tag, latitude, longitude, capture, min, max));
+
+            }
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return mPOIHashMap;
+    }
 }
-
-
